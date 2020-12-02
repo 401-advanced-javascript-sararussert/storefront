3bf04ba3cbb2d5c0bd766714a9e6087ca6747df7
SARA RUSSERT <sarabeth.russert@gmail.com>
2020-12-01 18:44:25 -0800

    adds models and sample data

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
-const { POINT_CONVERSION_COMPRESSED } = require('constants');
-const express = require('express');
-const products = require('./data/products');
+import dotenv from 'dotenv';
+import express from 'express';
+import colors from 'colors';
+import connectDB from './config/db.js';
+import products from './data/products.js';
+
+dotenv.config();
+
+connectDB();
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -14,4 +21,6 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 })
 
-app.listen(5000, console.log('server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold));
