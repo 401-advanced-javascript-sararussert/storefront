f2843f7008a89e24ce40ae2288f354db9137a22b
SARA RUSSERT <sarabeth.russert@gmail.com>
2021-01-06 16:35:44 -0800

    adds user name to the header if a user is logged in

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link as SiteLink } from 'react-router-dom';
 import { Breadcrumbs, Link, Typography, Container } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { logout } from '../actions/userActions';
+
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const dispatch = useDispatch()
+  const userLogin = useSelector(state => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
+  useEffect(() => {
+    setAnchorEl(null);
+  }, [userInfo])
+
   return (
     <header >
       <SiteLink to="/" className="linkStyle">
@@ -16,11 +44,27 @@ const Header = () => {
           CART
           </Link>
         </SiteLink>
-        <SiteLink to="/login" className="linkStyle">
-          <Link color="inherit"><i className='fas fa-user'></i>
-          SIGN IN
-          </Link>
-        </SiteLink>
+        {userInfo? (
+          <div>
+          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            {userInfo.name}
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <SiteLink to="/profile" className="linkStyle"><MenuItem onClick={handleClose}>Profile</MenuItem></SiteLink>
+            <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+          </Menu>
+        </div>
+        ) : <SiteLink to="/login" className="linkStyle">
+        <Link color="inherit"><i className='fas fa-user'></i>
+        SIGN IN
+        </Link>
+      </SiteLink>}
       </Breadcrumbs>
     </header>
   )
